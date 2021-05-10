9993ebdbcbd9195fcce6cc4a4f5bc615045e2c1b
noisersup <patekiapl@gmail.com>
2021-05-10 15:26:36 +0200

    🏷️ added response models

---
+++ models/models.go
@@ -0,0 +1,27 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type GetResponse struct{
+	Tasks	[]Task `json:"tasks"`
+	Events	[]Event `json:"events"`
+	Error	string `json:"error"`
+}
+
+type Task struct{
+	ID			primitive.ObjectID `bson:"_id" json:"id"`
+	Title		string `json:"title"`
+	Desc		string `json:"desc"`
+	Done		bool `json:"done"`
+	Order		int `json:"order"`
+	Due			int `json:"due"`
+}
+
+type Event struct{
+	ID			primitive.ObjectID `bson:"_id" json:"id"`
+	Title		string `json:"title"`
+	Desc		string `json:"desc"`
+	Order		int `json:"order"`
+	From		int `json:"from"`
+	Due			int `json:"due"`
+}
