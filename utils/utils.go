ae8d0252a241f10b6930f2eb93b266831e10fcbb
noisersup <patekiapl@gmail.com>
2021-05-11 08:45:03 +0200

    :building_construction: Added utils

---
+++ utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+)
+
+func Err(msg string, err error) error{
+	return errors.New("["+msg+"] "+err.Error())
+}
+
+func SendResponse(w http.ResponseWriter, response interface{}, statusCode int){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Printf("JSON encoding error: %s",err) //TODO: Log file
+	}
+}
