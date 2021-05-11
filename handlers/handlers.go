6e4768f8cfd36f8be725fd7f848a1b1cc940a27e
noisersup <patekiapl@gmail.com>
2021-05-11 09:55:00 +0200

    :sparkles: Connected calendar to tasks service

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,16 +2,32 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/noisersup/dashboard-backend-calendar/models"
 	"github.com/noisersup/dashboard-backend-calendar/utils"
 )
 
 type Server struct{
-	ip 			string
-	endpoint 	string
+	client		*http.Client
+	tasksUri 	*url.URL
+}
+
+func NewServer(tasksIp string, tasksPort int, tasksEndpoint string) *Server {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	
+	uri,err := url.Parse(fmt.Sprintf("http://%s:%d%s",tasksIp,tasksPort,tasksEndpoint))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Url: %s",uri)
+	return &Server{client: &client, tasksUri: uri}
 }
 
 func (s *Server) GetTasks(w http.ResponseWriter, r *http.Request){
@@ -19,7 +35,9 @@ func (s *Server) GetTasks(w http.ResponseWriter, r *http.Request){
 	
 	response := models.GetResponse{}
 	
-	reqRes, err := http.Get("http://"+s.ip+s.endpoint)
+	
+
+	reqRes, err := s.client.Get(s.tasksUri.String())
 	if err != nil { 
 		log.Print(err) 
 		utils.SendResponse(w,response,http.StatusInternalServerError)
--- main.go
+++ main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/noisersup/dashboard-backend-calendar/handlers"
 )
 
 func main(){
 	r := mux.NewRouter()
+	config := getVars()
+	s := handlers.NewServer(config.TasksIP,config.TasksPort,config.TasksEndpoint)
 
-	log.Fatal(http.ListenAndServe(":8000",r))
+	r.HandleFunc("/",s.GetTasks).Methods("GET")
+
+	log.Printf("Starting http server on port :%d...",config.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",config.Port),r))
+}
+
+type Config struct{
+	TasksIP 		string
+	TasksPort 		int
+	TasksEndpoint 	string
+	Port 			int
+}
+
+func getVars() *Config {	
+	var config Config
+
+	config.TasksIP = os.Getenv("TASKS_IP")
+	config.TasksEndpoint = os.Getenv("TASKS_ENDPOINT")
+	config.TasksPort,_ = strconv.Atoi(os.Getenv("TASKS_PORT"))
+	config.Port,_ = strconv.Atoi(os.Getenv("PORT")) //default: 8000
+
+	if config.TasksIP=="" || config.TasksPort== 0 || config.TasksEndpoint == ""{ 
+		log.Fatal("ENV variables did not set")
+	}
+
+	if config.Port == 0 {
+		config.Port = 8000
+		log.Printf("PORT variable is not set. loading defaults: %d",config.Port)
+	}
+
+	return &config
 }
